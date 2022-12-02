3699f71289c28f2f1c93dbf1447d66e7e32fe89c
João <joaoquiterio.ps3@gmail.com>
2022-12-02 12:41:49 +0000

    Login

--- flaskWebsite.py
+++ flaskWebsite.py
@@ -1,33 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 
 app = Flask(__name__, static_folder='staticFiles', template_folder='templates')
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
-db = SQLAlchemy()
+Session(app)
+
+db = SQLAlchemy(app)
 db.init_app(app)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)   
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String)
+    email = db.Column(db.String(50))
 
 @app.route("/")
 def index():
-    dbuser = db.engine.execute('SELECT * FROM `user`')
-    print(dbuser.all())
+    dbuser = db.session.query(User).all()
+    # print(dbuser)
+    for user in dbuser:
+        print(user.email)
     return render_template('index.html')
 
 @app.route("/404")
 def error404():
     return render_template('404.html')
 
-@app.route("/login")
-def login():    
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form.get('login-form-username')
+        password = request.form.get('login-form-password')
+
+        if User.query.filter_by(username = user).filter_by(password = password).first():
+            session['username'] = request.form.get('login-form-username')
+            return redirect(url_for('profile'))
+    
     return render_template('login.html')
 
 @app.route("/profile")
 def profile():
+    if not session.get("username"):
+        return redirect(url_for('login'))
     return render_template('profile.html')
 
 @app.route("/cart")
